379d6431ddf73cbfd06c8183fd23152a99ea185a
Chad Monroe <chad@monroe.io>
2025-09-20 05:09:06 -0700

    SDG-4716: add device tree for SDG-8716v

--- airoha/image/smartrg.mk
+++ airoha/image/smartrg.mk
@@ -15,6 +15,7 @@ define Device/dragontail
   DEVICE_VENDOR := SmartRG
   DEVICE_MODEL := SmartRG Target
   DEVICE_DTS := an7581-smartrg-dragontail
+  DEVICE_DTS += an7581-smartrg-SDG-8716v
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-i2c-an7581
   ARTIFACT/preloader.bin := an7581-preloader smartrg_dragontail
@@ -90,7 +91,7 @@ define Build/SrgFit
 	   	-i "k1" -k $@ -a $(KERNEL_LOADADDR) -e $(if $(KERNEL_ENTRY),$(KERNEL_ENTRY),$(KERNEL_LOADADDR)) -C lzma -h "crc32" -h "sha1" \
 		-i "rdisk" -r $(STAGING_DIR_IMAGE)/$(IMG_PREFIX)-initramfs.cpio.gz -h "crc32" -h "sha1" \
 		-i "SDG-an7581-rfb" -d $(KDIR)/image-an7581-smartrg-dragontail.dtb -h "crc32" -h "sha1" \
-		-i "SDG-8716v" -d $(KDIR)/image-an7581-smartrg-dragontail.dtb -h "crc32" -h "sha1" \
+		-i "SDG-8716v" -d $(KDIR)/image-an7581-smartrg-SDG-8716v.dtb -h "crc32" -h "sha1" \
 		-c "303" -K k1 -R rdisk -D "SDG-an7581-rfb" \
 		-c "600" -K k1 -R rdisk -D "SDG-8716v"
 
